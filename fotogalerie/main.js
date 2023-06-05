cd908360b97f626a5adb2930cc01c4e8987366b6
FV <vecekspse@gmail.com>
2023-06-05 14:12:47 +0200

    rework

--- fotogalerie/main.js
+++ fotogalerie/main.js
@@ -1,1 +1,23 @@
 "use strict"
+let pocitadlo = 0;
+
+const obrazky = document.querySelectorAll(".thumbnails img");
+const mainImg = document.querySelector(".main-image img");
+
+const next = document.querySelector(".next");
+next.addEventListener("click", function () {
+    pocitadlo++;
+    if(pocitadlo > obrazky.length - 1) {
+        pocitadlo = 0;
+    }
+    mainImg.src = obrazky[pocitadlo].src;
+});
+
+const prev = document.querySelector(".prev");
+prev.addEventListener("click", function () {
+    pocitadlo--;
+    if(pocitadlo < 0) {
+        pocitadlo = obrazky.length -1;
+    }
+    mainImg.src = obrazky[pocitadlo].src;
+});
