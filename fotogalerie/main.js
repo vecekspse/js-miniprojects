a2653e7f98d4959fda27c4fdbc20be5b3b5d0438
FV <vecekspse@gmail.com>
2023-06-05 15:33:54 +0200

    go to hub

--- fotogalerie/main.js
+++ fotogalerie/main.js
@@ -1,9 +1,16 @@
 "use strict"
 
+let pocitadlo = 0;
+
 const mainImg = document.querySelector(".main-image img");
+const obrazky = document.querySelectorAll(".thumbnails img");
 
 const next = document.querySelector(".controls .next");
 
 next.addEventListener("click", function () {
-    mainImg.src = "images/image6.jpg";
+    pocitadlo++;
+    if(pocitadlo > obrazky.length - 1) {
+        pocitadlo = 0;
+    }
+    mainImg.src = obrazky[pocitadlo].src;
 });
