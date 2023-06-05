b06c0901bf9825f5350b5cbb21bfc644b86bc376
FV <vecekspse@gmail.com>
2023-06-05 15:07:43 +0200

    blabal

--- fotogalerie/main.js
+++ fotogalerie/main.js
@@ -1,1 +1,9 @@
 "use strict"
+
+const mainImg = document.querySelector(".main-image img");
+
+const next = document.querySelector(".controls .next");
+
+next.addEventListener("click", function () {
+    mainImg.src = "images/image6.jpg";
+});
